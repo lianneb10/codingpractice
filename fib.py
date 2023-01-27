11c57c36d2db327cadec7c44b0d3fd864aa1fce2
lbaratea <aratealianne@gmail.com>
2023-01-26 16:18:59 -0800

    start

---
+++ fib.py
@@ -0,0 +1,19 @@
+n='hello'
+print(n)
+
+def practice(name):
+   return 'hello ' + name
+
+print(practice('lianne')+"!")
+
+def square(number):
+    return number*number
+
+print(square(5)+square(12))
+
+total=0
+for i in range (1,11):
+    total = total +i
+ 
+print(total)
+    
