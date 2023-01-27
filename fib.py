af4c9414bd5ca730601ee498633ee09406155de2
lbaratea <aratealianne@gmail.com>
2023-01-26 16:40:00 -0800

    fib

--- fib.py
+++ fib.py
@@ -17,3 +17,26 @@ for i in range (1,11):
  
 print(total)
     
+def added(number):
+    if number == 1:
+        print('the sum of 1 is 1')
+        return 1
+    else: 
+        print('trying to find the sum of ' + str(number) + 'we are going to add it to '+ str(number-1))
+        return number + added(number-1) 
+
+# print(added(10))
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n ==1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+
+print(fib(10))
+
+for i in range (1,10):
+    print('the fib of ' +  str(i) + ' is ' + str(fib(i)))
